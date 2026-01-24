89f5006f7e145d1a75c0055c3a36b16f11f7f6fc
Jonna Huotari <t1hujo04@students.oamk.fi>
2026-01-24 20:37:26 +0200

    Firebase config luominen

---
+++ Config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcEAgqH42LpdUb83P5AddTc6aTi17DIBY",
+  authDomain: "workbalance-859f1.firebaseapp.com",
+  projectId: "workbalance-859f1",
+  storageBucket: "workbalance-859f1.firebasestorage.app",
+  messagingSenderId: "591603411256",
+  appId: "1:591603411256:web:deb4db9d6adc02288db9cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
