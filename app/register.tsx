defb79f85b882af2cb8303542da1b32b3c8e7c30
Jonna Huotari <t1hujo04@students.oamk.fi>
2026-02-07 18:39:06 +0200

    Firebase yhteys

--- app/register.tsx
+++ app/register.tsx
@@ -1,45 +1,75 @@
-import { router } from 'expo-router';
-import { useState } from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import { db, collection, addDoc, serverTimestamp } from "../Config"; 
+import { useRouter } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function Register() {
-  const [username, setUsername] = useState("");
+  const [title, setTitle] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
+  const router = useRouter();
+
   const handleBackToLogin = () => {
-    router.push('/');
+    router.push("/");
+  }
+
+  const handleRegister = async () => {
+    if (!email || !password || !title) {
+      Alert.alert("Virhe", "Täytä kaikki kentät");
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, "users"), {
+        email: email,
+        password: password, 
+        title: title,
+        createdAt: serverTimestamp(),
+      });
+
+      Alert.alert("Onnistui", "Käyttäjä luotu");
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Virhe", "Käyttäjän luonti epäonnistui");
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Rekisteröinti!</Text>
+      <Text style={styles.title}>Rekisteröinti</Text>
+
       <TextInput
-              style={styles.input}
-              placeholder="Käyttäjätunnus"
-              value={username}
-              onChangeText={setUsername}
-              autoCapitalize="none"
-            />
-      
-            <TextInput
-              style={styles.input}
-              placeholder="Salasana"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Sähköposti"
-              value={email}
-              onChangeText={setEmail}
-              autoCapitalize="none"
-            />
-      <Button title="Takaisin kirjautumiseen" onPress={() => handleBackToLogin()} />
+        style={styles.input}
+        placeholder="Sähköposti"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Salasana"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Titteli"
+        value={title}
+        onChangeText={setTitle}
+      />
+
+      <Button title="Luo käyttäjä" onPress={handleRegister} />
+      <Button title="Takaisin kirjautumiseen" onPress={handleBackToLogin} />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
